b3350e9e12eea44c06a194b8a7ffc87dec66633c
richardhenyash <richardash1@gmail.com>
2022-04-11 08:01:07 +0100

    Refactor props

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -11,8 +11,11 @@ class AddPlayerForm extends Component {
     }
 
     handleSubmit = (e) => {
+        const {
+            addPlayer
+        } = this.props
         e.preventDefault();
-        this.props.addPlayer(this.state.value);
+        addPlayer(this.state.value);
         this.setState({ value: ''})
     }
 
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Counter from './Counter';
 
-const Player = (props) => {
+class Player extends PureComponent {
+  render() {
+    const {
+      name,
+      id,
+      score,
+      index,
+      removePlayer,
+      changeScore
+    } = this.props
+    console.log(name + ' rendered');
     return (
       <div className="player">
         <span className="player-name">
-          <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-          { props.name }
+          <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+          { name }
         </span>
   
         <Counter 
-          score={props.score}
-          index={props.index}
-          changeScore={props.changeScore}
+          score={score}
+          index={index}
+          changeScore={changeScore}
         />
       </div>
     );
-  }
+  }    
+}
 
 export default Player;
